cfb61693f1376e6505a7c33d68f728b290ed9cc7
James F. Thomas <james.fthomas@yahoo.com>
2021-03-06 13:00:02 -0600

    (add) database routes with dummy routine created and tested with postman

--- server/index.js
+++ server/index.js
@@ -1,14 +1,17 @@
-// require package to allow use of .env file
-require('dotenv').config();
-
 // require express for application server
 const express = require('express');
 
-// require package that service static front end files ./client/dist
-const path = require('path');
+// require package to allow use of .env file
+require('dotenv').config();
 
 // require DbFunctions file so they can be used/tested
 require('./dbFunctions');
+
+//require dbRoutes file
+const dbRoutes = require('./dbRoutes')
+
+// require package that service static front end files ./client/dist
+const path = require('path');
 
 // set the port number for the application server
 const port = 3000
@@ -21,6 +24,9 @@ const app = express()
 
 // use express package static middleware to server front end files
 app.use(express.static(frontEnd))
+
+// tell express package to use dbRoutes to respond to all db requests
+app.use('/db', dbRoutes )
 
 // use express listen function to invoke server instance, set to PORT for requests
 app.listen(port, () => console.info(`http://localhost:${port}`))
